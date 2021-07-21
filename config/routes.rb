1dd32959cd3b5fa1c650d577e3062eff00461287
hi08ki <mop__clean@ezweb.ne.jp>
2021-07-21 14:22:58 +0900

    registrations/sessions/new.html.erbの書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'items#index' 
+  # resources :user
 
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  # get 'users', to: 'users#index'
+  # get 'users/new', to: 'users#new'
 end
