238f9c508228c17aaee5d53e5b361f9957020162
hi08ki <mop__clean@ezweb.ne.jp>
2021-07-23 17:48:15 +0900

    バリデーションの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
     validates :nickname
     validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: "Please input letters and numbers"}
   end
-  with_options presence: true do, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/} do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/} do
     validates :last_name
     validates :first_name
   end
-  with_options presence: true do, format: {with: /\A[ァ-ヶー－]+\z/ }
+  with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } do
     validates :last_name_kana
     validates :first_name_kana
     validates :birthday
