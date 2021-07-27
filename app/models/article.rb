976d883e564313540f015cc99d2c608941c769b6
hi08ki <mop__clean@ezweb.ne.jp>
2021-07-27 17:16:13 +0900

    販売手数料の計算

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,16 @@
 class Article < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+  belongs_to :burden 
+  belongs_to :category
+  belongs_to :daysdelivery
+  belongs_to :delivery
+  belongs_to :status
 
   validates :title, :text, presence: true
 
-  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :burden_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :days_delivery_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}  
 end
