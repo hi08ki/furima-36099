78ad09a87f0022da1bae3541f88f3cc1642e1fcd
hi08ki <mop__clean@ezweb.ne.jp>
2021-07-28 20:39:38 +0900

    最終修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,8 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/new'
-  devise_for :users,controllers: {
-    registrations: 'users/registrations',
-  }
-  devise_scope :user do
-    get 'sending_destinations', to: 'users/registrations#newSendingDestination'
-    post 'sending_destinations', to: 'users/registrations#createSendingDestination'
-  end
-
-
   devise_for :users
   root to: 'items#index'
   # resource :user,only: [:show, :index, :edit, :update] 
   resources :items,only: [:index, :new, :create]
-  
-  end
 end
 
 
