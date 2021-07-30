ea397f87c878081f0d7e146693ad05ff261f30a5
hi08ki <mop__clean@ezweb.ne.jp>
2021-07-30 19:19:39 +0900

    条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:create]
+  before_action :authenticate_user!, except: [:index,:new,:create,:show,:update]
+  before_action :contributor_confirmation, only: [:edit, :update]
+  # before_action :move_to_index, except: [:index, :show]
   
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -22,10 +24,29 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end 
 
+  def edit
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to items_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:product_name, :description, :category_id, :status_id, :burden_id, :delivery_id, :days_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    unless user_signed_in?
+     redirect_to root_path
+    end
+  end
 end
