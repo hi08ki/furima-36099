1e8da1013fa3d496ed9f1098d138728344919df6
hi08ki <mop__clean@ezweb.ne.jp>
2021-08-02 15:52:51 +0900

    コントローラーの編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,32 @@
 class PurchasesController < ApplicationController
+  def index
+    # Formオブジェクトのインスタンスを作成して、インスタンス変数に代入する
+    @user_item = UserItem.new
+  end
+
+  def new
+  end
+
+  def create
+    user = User.create(user_params)
+    Residence.create(residence_params(user))
+    Purchase.create(purchase_params(user))
+    redirect_to action: :index
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :name_reading, :nickname)
+  end
+
+#   def address_params(user)
+#     params.permit(:postal_code, :prefecture, :city, :house_number, :building_name).merge(user_id: user.id)
+#   end
+
+#   def investment_params(user)
+#     params.permit(:item).merge(user_id: user.id)
+#   end
+# end
+
 end
