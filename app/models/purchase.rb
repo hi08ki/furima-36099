98f557923b5dd33ec493725db817aee4e4c99395
hi08ki <mop__clean@ezweb.ne.jp>
2021-08-02 13:38:53 +0900

    モデル作成

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,18 +1,2 @@
-class Purchase
-  include ActiveModel::Model
-  attr_accessor :user,:item
-
-  belongs_to :user
-  belongs_to :item
-  has_one :residence
-  # ここにバリデーションの処理を書く
-
-  with_options presence: true do
-    validates :user
-    validates :item
-
-  def save
-    # ユーザーの情報を保存し、「user」という変数に代入
-    user = User.create(nickname: nickname, last_name: last_name, farst_name: farst_name, last_name_kana: last_name_kana, farst_name_kana: farst_name_kana)
-  end
+class Purchase < ApplicationRecord
 end
--- app/models/residence.rb
+++ app/models/residence.rb
@@ -1,23 +1,2 @@
-class Residence
-  include ActiveModel::Model
-  attr_accessor :purchase,:postal_code,:delively_id,:municipality,:address,:building_name,:phone_number
-
-belongs_to :purchase
- 
-with_options presence: true do
-  validates :purchase
-  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" } 
-  validates :delively_id
-  validates :municipality
-  validates :address
-  validates :building_name
-  validates :phone_number
-
-
-  def save
-    # 住所の情報を保存
-    Address.create(postal_code: postal_code, delivery＿id: delivery_id, municipality: municipality, address: address, building_name: building_name, building_name: building_name, user_id: user.id)
-    # 投資金の情報を保存
-    Investment.create(price: price, user_id: user.id)
-  end
+class Residence < ApplicationRecord
 end
