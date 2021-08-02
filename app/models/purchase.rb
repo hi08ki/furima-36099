8465ca0e4d4d2127820db7314742f7b43a842645
hi08ki <mop__clean@ezweb.ne.jp>
2021-08-02 13:06:05 +0900

    バリデーションの設定

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,12 +1,17 @@
 class Purchase
   include ActiveModel::Model
-  attr_accessor :hoge,:fuga...
+  attr_accessor :user,:item
 
   belongs_to :user
   belongs_to :item
   has_one :residence
   # ここにバリデーションの処理を書く
 
+  with_options presence: true do
+    validates :user
+    validates :item
+    
   def save
     # 各テーブルにデータを保存する処理を書く
   end
+end
--- app/models/residence.rb
+++ app/models/residence.rb
@@ -1,9 +1,18 @@
 class Residence
   include ActiveModel::Model
-  attr_accessor :hoge,:fuga...
+  attr_accessor :purchase,:postal_code,:delively_id,:municipality,:address,:building_name,:phone_number
 
 belongs_to :purchase
-  # ここにバリデーションの処理を書く
+ 
+with_options presence: true do
+  validates :purchase
+  validates :postal_code
+  validates :delively_id
+  validates :municipality
+  validates :address
+  validates :building_name
+  validates :phone_number
+
 
   def save
     # 各テーブルにデータを保存する処理を書く
